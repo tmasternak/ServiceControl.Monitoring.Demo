a29ca21848706dd13f633c07db29871633445619
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-07-25 13:42:24 +0200

    fixing typo

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 start .\binaries\SubscriberA\Subscriber.exe
-start .\binaries\SubscriberA\Subscriber.exe
+start .\binaries\SubscriberB\Subscriber.exe
 start .\binaries\Publisher\Publisher.exe
 
-REM start c:\git\p\sc.monitoring\binaries\ServiceControl.Monitoring.exe
-REM start c:\git\p\ServicePulse\src\ServicePulse.Host\bin\Debug\ServicePulse.Host.exe
+start c:\git\p\sc.monitoring\binaries\ServiceControl.Monitoring.exe
+start c:\git\p\ServicePulse\src\ServicePulse.Host\bin\Debug\ServicePulse.Host.exe
